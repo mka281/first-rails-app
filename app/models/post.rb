b390b08c31a26c6e5c0c5ad1871542f1bf16544e
mka <mka281@icloud.com>
2018-05-15 01:04:39 +0300

    Use paperclip gem to add photos to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
